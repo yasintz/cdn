62693ef9cca7ecd2ef1401fa7a5afeb3bc4dcbc0
Yasin Tazeoglu <yasinntazeoglu@gmail.com>
2020-08-14 11:22:43 +0300

    Update tureng-site-opener.js

--- tureng-site-opener.js
+++ tureng-site-opener.js
@@ -1,58 +1,62 @@
-const mousePosition = { x: 0, y: 0 };
+function turengTooltip(onClick) {
+  const mousePosition = { x: 0, y: 0 };
 
-const ALIGMENT_LEFT = 60;
-const ALIGMENT_TOP = 30;
-const isMediumPage = window.location.href.indexOf('medium') === 8;
+  const ALIGMENT_LEFT = 60;
+  const ALIGMENT_TOP = 30;
+  const isMediumPage = window.location.href.indexOf('medium') === 8;
 
-const TURENG_SITE_OPENER_CLASSNAME = 'tureng-site-opener-xxx';
-const TurengSiteOpener = document.createElement('a');
-TurengSiteOpener.classList.add(TURENG_SITE_OPENER_CLASSNAME);
+  let selectedWord = '';
 
-loadCss(`
-.${TURENG_SITE_OPENER_CLASSNAME}{
-  background-color: #ddd;
-  border: 1px solid #ddd;
-  position: absolute;
-}
-`);
+  const TURENG_SITE_OPENER_CLASSNAME = 'tureng-site-opener-xxx';
+  const TurengSiteOpener = document.createElement('button');
+  TurengSiteOpener.classList.add(TURENG_SITE_OPENER_CLASSNAME);
 
-TurengSiteOpener.innerHTML = 'Tureng';
-TurengSiteOpener.target = '_blank';
+  loadCss(`
+    .${TURENG_SITE_OPENER_CLASSNAME}{
+    background-color: #ddd;
+    border: 1px solid #ddd;
+    position: absolute;
+    }
+  `);
 
-function getSelectionText() {
-  var text = '';
-  if (window.getSelection) {
-    text = window.getSelection().toString();
-  } else if (document.selection && document.selection.type != 'Control') {
-    text = document.selection.createRange().text;
-  }
-  return text;
-}
+  TurengSiteOpener.innerHTML = 'Tureng';
+  TurengSiteOpener.onclick = () => onClick(selectedWord);
 
-function buildElement(searchText) {
-  const { x, y } = mousePosition;
-
-  TurengSiteOpener.style.top = (y < ALIGMENT_TOP ? y : y - ALIGMENT_TOP) + 'px';
-  if (isMediumPage) {
-    TurengSiteOpener.style.top = y + ALIGMENT_TOP + 'px';
-  }
-  TurengSiteOpener.style.left =
-    (x < ALIGMENT_LEFT ? x + 350 : x - ALIGMENT_LEFT) + 'px';
-  TurengSiteOpener.href = `https://tureng.com/tr/turkce-ingilizce/${searchText}`;
-
-  return TurengSiteOpener;
-}
-
-function onTextSelected(selectedText) {
-  const hasMultipleWord = selectedText.split(' ').length > 1;
-  if (hasMultipleWord) {
-    return;
+  function getSelectionText() {
+    var text = '';
+    if (window.getSelection) {
+      text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != 'Control') {
+      text = document.selection.createRange().text;
+    }
+    return text;
   }
 
-  document.body.appendChild(buildElement(selectedText));
-}
+  function setPosition() {
+    const { x, y } = mousePosition;
 
-function main() {
+    TurengSiteOpener.style.top =
+      (y < ALIGMENT_TOP ? y : y - ALIGMENT_TOP) + 'px';
+    if (isMediumPage) {
+      TurengSiteOpener.style.top = y + ALIGMENT_TOP + 'px';
+    }
+    TurengSiteOpener.style.left =
+      (x < ALIGMENT_LEFT ? x + 350 : x - ALIGMENT_LEFT) + 'px';
+
+    return TurengSiteOpener;
+  }
+
+  function onTextSelected(selectedText) {
+    const hasMultipleWord = selectedText.split(' ').length > 1;
+    if (hasMultipleWord) {
+      return;
+    }
+
+    selectedWord = selectedText;
+
+    document.body.appendChild(setPosition(selectedText));
+  }
+
   window.addEventListener('mouseup', (event) => {
     const isTurengElement = event.target === TurengSiteOpener;
     const selection = getSelectionText().trim();
@@ -80,4 +84,51 @@ function main() {
   });
 }
 
-main();
+function createTurengOpenerListener() {
+  loadScript(
+    'https://raw.githubusercontent.com/yasintz/cdn/master/handle-keyboard-event.js',
+    () => {
+      keyboardListener(({ o, withAlt, withCtrl }) => {
+        if (withCtrl && withAlt && o) {
+          turengTooltip((word) => {
+            window.open(`https://tureng.com/tr/turkce-ingilizce/${word}`);
+          });
+        }
+      });
+    }
+  );
+}
+
+function createTurengDispatcherListener() {
+  loadScript(
+    'https://raw.githubusercontent.com/yasintz/cdn/master/handle-keyboard-event.js',
+    () => {
+      const EVENT_NAME = 'TURENG_WORD_DISPATCHER';
+
+      keyboardListener(({ t, withAlt, withCtrl }) => {
+        if (withCtrl && withAlt && t) {
+          loadScript(
+            'https://zn-socket.vercel.app/socket.io/socket.io.js',
+            () => {
+              const socket = io();
+              turengTooltip((word) => {
+                socket.emit(EVENT_NAME, word);
+              });
+            }
+          );
+        }
+      });
+    }
+  );
+}
+
+$l(() => {
+  const isTurengPage = window.location.href.indexOf('tureng') === 8;
+
+  if (isTurengPage) {
+    return;
+  }
+
+  createTurengDispatcherListener();
+  createTurengOpenerListener();
+});
