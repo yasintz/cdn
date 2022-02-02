3482eee181df30002eecb1fa81a82b293fa5461a
Yasin Tazeoglu <yasinntazeoglu@gmail.com>
2022-02-03 01:21:27 +0300

    Update index.js

--- vue-css/index.js
+++ vue-css/index.js
@@ -1,82 +1,119 @@
-const getHashCode = s =>
-  s.split("").reduce((a, b) => {
-    a = (a << 5) - a + b.charCodeAt(0);
-    return a & a;
-  }, 0);
+function VueCss() {
+  const getHashCode = s =>
+    s.split("").reduce((a, b) => {
+      a = (a << 5) - a + b.charCodeAt(0);
+      return a & a;
+    }, 0);
 
-const el = document.createElement("style");
+  const el = document.createElement("style");
 
-const cache = {};
+  const cache = {};
 
-function css(strList, ...vars) {
-  if (typeof strList === "string") {
-    return cache[strList];
+  function cx() {
+    var hasOwn = {}.hasOwnProperty;
+    var classes = [];
+
+    for (var i = 0; i < arguments.length; i++) {
+      var arg = arguments[i];
+      if (!arg) continue;
+
+      var argType = typeof arg;
+
+      if (argType === "string" || argType === "number") {
+        classes.push((this && this[arg]) || arg);
+      } else if (Array.isArray(arg)) {
+        classes.push(classNames.apply(this, arg));
+      } else if (argType === "object") {
+        if (arg.toString === Object.prototype.toString) {
+          for (var key in arg) {
+            if (hasOwn.call(arg, key) && arg[key]) {
+              classes.push((this && this[key]) || key);
+            }
+          }
+        } else {
+          classes.push(arg.toString());
+        }
+      }
+    }
+
+    return classes.join(" ");
   }
 
-  const content = strList.reduce(
-    (acc, cur, index) =>
-      `${acc}${cur}${index < strList.length - 1 ? vars[index] : ""}`,
-    ""
-  );
+  function css(strList, ...vars) {
+    if (typeof strList === "string") {
+      return cache[strList];
+    }
 
-  const minContent = content
-    .replace(/\n/g, " ")
-    .replace(/ +/g, " ")
-    .replace(/\/\*.+?\*\//gs, "");
+    const content = strList.reduce(
+      (acc, cur, index) =>
+        `${acc}${cur}${index < strList.length - 1 ? vars[index] : ""}`,
+      ""
+    );
 
-  const onlySelectors = minContent.replace(/{(.*?)}/g, " ");
+    const minContent = content
+      .replace(/\n/g, " ")
+      .replace(/ +/g, " ")
+      .replace(/\/\*.+?\*\//gs, "");
 
-  const allClasses =
-    onlySelectors.match(
-      /(?:[\.]{1})([a-zA-Z_]+[\w-_]*)(?:[\s\.\,\{\>#\:]{0})/gim
-    ) || [];
+    const onlySelectors = minContent.replace(/{(.*?)}/g, " ");
 
-  const uniqueClasses = allClasses.filter((v, i, s) => s.indexOf(v) === i);
+    const allClasses =
+      onlySelectors.match(
+        /(?:[\.]{1})([a-zA-Z_]+[\w-_]*)(?:[\s\.\,\{\>#\:]{0})/gim
+      ) || [];
 
-  const orderedByLength = uniqueClasses.sort((a, b) => b.length - a.length);
+    const uniqueClasses = allClasses.filter((v, i, s) => s.indexOf(v) === i);
 
-  const hashMap = orderedByLength.map(c => ({
-    className: c.substring(1),
-    hash: `jss_${getHashCode(c)}`,
-    class: c
-  }));
+    const orderedByLength = uniqueClasses.sort((a, b) => b.length - a.length);
 
-  const newContent = hashMap.reduce(
-    (acc, cur) => acc.split(cur.class).join(`.${cur.hash}`),
-    content
-  );
+    const hashMap = orderedByLength.map(c => ({
+      className: c.substring(1),
+      hash: `jss_${getHashCode(c)}`,
+      class: c
+    }));
 
-  const accessObject = hashMap.reduce((acc, cur) => {
-    acc[cur.className] = cur.hash;
-    return acc;
-  }, {});
+    const newContent = hashMap.reduce(
+      (acc, cur) => acc.split(cur.class).join(`.${cur.hash}`),
+      content
+    );
 
-  const idRegex = /\/\* \#(.*?) \*\//g;
-  const firstLine = content
-    .split("\n")
-    .map(i => i.trim())
-    .filter(i => i)[0];
+    const accessObject = hashMap.reduce((acc, cur) => {
+      acc[cur.className] = cur.hash;
+      return acc;
+    }, {});
 
-  const id = (new RegExp(idRegex).exec(firstLine) || [])[1];
+    accessObject.cx = cx.bind(accessObject);
 
-  if (id) {
-    const idHash = getHashCode(`jss_${id}_jss`);
-    const prefix = `/*${idHash}*/`;
+    const idRegex = /\/\* \#(.*?) \*\//g;
+    const firstLine = content
+      .split("\n")
+      .map(i => i.trim())
+      .filter(i => i)[0];
 
-    const [prev, old, after] = el.innerHTML.split(prefix);
-    el.innerHTML = [
-      //
-      prev,
-      after,
-      prefix,
-      newContent,
-      prefix
-    ].join("\n");
+    const id = (new RegExp(idRegex).exec(firstLine) || [])[1];
 
-    cache[id] = accessObject;
+    if (id) {
+      const idHash = getHashCode(`jss_${id}_jss`);
+      const prefix = `/*${idHash}*/`;
+
+      const [prev, old, after] = el.innerHTML.split(prefix);
+      el.innerHTML = [
+        //
+        prev,
+        after,
+        prefix,
+        newContent,
+        prefix
+      ].join("\n");
+
+      cache[id] = accessObject;
+    }
+
+    return accessObject;
   }
-
-  return accessObject;
+  css.cx = id => {
+    return (...args) => cx.bind(cache[id])(...args);
+  };
+  this.css = css;
+  this.el = el;
 }
-
-document.head.appendChild(el);
