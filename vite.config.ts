f312ea9b332d74f6e4d23ffb5040b915ce8e08f9
Yasin Tazeoglu <yasinntazeoglu@gmail.com>
2023-01-15 02:37:36 +0300

    v1

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, PluginOption, ViteDevServer } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+
+function serveHtml(): PluginOption {
+  return {
+    apply: 'serve',
+    configureServer(server: ViteDevServer) {
+      return () => {
+        server.middlewares.use(async (req, res, next) => {
+          if (req.originalUrl?.endsWith('html')) {
+            res.setHeader('Content-Type', 'text/html');
+            res.writeHead(200);
+            res.write(
+              fs.readFileSync(path.join(__dirname, `public/${req.originalUrl}`))
+            );
+            res.end();
+          }
+
+          next();
+        });
+      };
+    },
+    name: 'charting-library',
+  };
+}
+
+export default defineConfig({
+  plugins: [react()],
+});
