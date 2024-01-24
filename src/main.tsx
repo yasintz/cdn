bce7ed1b667fc13e852b3a5fad583a7f7a0a2bdb
Yasin <yasinntazeoglu@gmail.com>
2024-01-24 22:23:11 +0300

    removed

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Router from './router';
-
-const element = document.getElementById('root') as HTMLElement;
-const root = ReactDOM.createRoot(element);
-
-root.render(<Router />);
