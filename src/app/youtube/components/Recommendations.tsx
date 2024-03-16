b722f06cdecdada075d0a327cad0c45a4ac6db0e
Yasin <yasinntazeoglu@gmail.com>
2024-03-16 19:13:40 +0300

    Updates

--- src/app/youtube/components/Recommendations.tsx
+++ src/app/youtube/components/Recommendations.tsx
@@ -1,12 +1,14 @@
 import ImageCard from './ImageCard';
 import { generate } from 'random-words';
+import { useState } from 'react';
 import { toast } from 'sonner';
 
-const word = generate() as string;
+// const word = generate() as string;
 
 const Recommendations = () => {
+  const [word, setWord] = useState(generate() as string);
   return (
-    <div className="flex gap-8 p-8">
+    <div className="flex gap-8 p-8 flex-wrap">
       <ImageCard
         title="Podcast Dinle"
         image="https://cdn.dribbble.com/users/730703/screenshots/11076980/media/f092bb64b0c5458e962cddfa9d3ff272.jpg"
@@ -17,9 +19,19 @@ const Recommendations = () => {
       <ImageCard
         title="Yeni Bir Kelime Ogren"
         image="https://img.freepik.com/free-vector/hand-drawn-sustainable-travel-illustration_52683-148456.jpg?w=1060&t=st=1710570244~exp=1710570844~hmac=b017bf4439f717d98e9c2eec53097b958ea9981656bd93e78488df3dec78f22c"
-        description="Buyuk seyler kucuk adimlar ile basirilir"
-        text="Ogren"
-        to={`https://dictionary.cambridge.org/dictionary/english/${word}`}
+        text="Degistir"
+        description={
+          <>
+            Buyuk seyler kucuk adimlar ile basirilir.{' '}
+            <a
+              className="text-blue-500 underline"
+              href={`https://dictionary.cambridge.org/dictionary/english/${word}`}
+            >
+              <span className="font-bold">{word}</span>
+            </a>
+          </>
+        }
+        onClick={() => setWord(generate() as string)}
       />
       <ImageCard
         title="Dizi Yada Film Izle"
