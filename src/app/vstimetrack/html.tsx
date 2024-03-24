a95f0836246eddd10ea2cdf3560fa6126a2c957a
Yasin <yasinntazeoglu@gmail.com>
2024-03-24 05:43:26 +0300

    updates

--- src/app/vstimetrack/html.tsx
+++ src/app/vstimetrack/html.tsx
@@ -4,10 +4,10 @@ const Html = (props: PageHtmlProps) => {
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" href="/youtube/manifest.json" />
-        <link rel="icon" type="image/png" href="/youtube/icon.png" />
+        <link rel="manifest" href="/vstimetrack/manifest.json" />
+        <link rel="icon" type="image/png" href="/vstimetrack/icon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Vscode Time Tracker</title>
+        <title>VsTimeTrack</title>
       </head>
       <body>
         <div id="root" />
