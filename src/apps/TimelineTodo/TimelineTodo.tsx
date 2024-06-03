f2173b86d9cf739a20eec0e0d46b29e4d70bb919
Yasin <yasinntazeoglu@gmail.com>
2024-06-03 23:57:18 +0300

    update todo list

--- src/apps/TimelineTodo/TimelineTodo.tsx
+++ src/apps/TimelineTodo/TimelineTodo.tsx
@@ -7,8 +7,12 @@ import {
   ChevronUpIcon,
   EyeIcon,
   PencilIcon,
+  PlusCircleIcon,
   PlusIcon,
   TrashIcon,
+  XCircle,
+  XCircleIcon,
+  XIcon,
 } from 'lucide-react';
 import { Checkbox } from '@/components/ui/checkbox';
 import dayjs from 'dayjs';
@@ -16,6 +20,7 @@ import duration from 'dayjs/plugin/duration';
 import _ from 'lodash';
 import { cn } from '@/lib/utils';
 import { withStoreLoading } from '@/utils/zustand/gsheet-storage';
+import { useMemo } from 'react';
 
 dayjs.extend(duration);
 
@@ -37,6 +42,7 @@ const TimelineTodo = () => {
     deleteSession,
     deleteEntry,
     deleteTodo,
+    reorderTodo,
   } = useStore();
 
   const handleCreateSession = () => {
@@ -64,10 +70,22 @@ const TimelineTodo = () => {
   };
 
   const session = sessions.find((session) => session.id === sessionId);
-  const sessionEntries = _.orderBy(
-    entries.filter((entry) => entry.sessionId === sessionId),
-    'time'
-  );
+  const sessionEntries = useMemo(() => {
+    const sessionEntriesRaw = entries.filter(
+      (entry) => entry.sessionId === sessionId
+    );
+
+    const sessionEntriesData = sessionEntriesRaw.map((item, index) => {
+      const nextItem = sessionEntriesRaw[index + 1];
+
+      return {
+        ...item,
+        diff: nextItem?.time - item.time,
+      };
+    });
+
+    return _.orderBy(sessionEntriesData, 'time');
+  }, [entries, sessionId]);
 
   return (
     <div className="p-3">
@@ -96,6 +114,16 @@ const TimelineTodo = () => {
           <PlusIcon size={14} className="mr-2" />
           Add Session
         </Button>
+        <Button
+          variant="secondary"
+          size="sm"
+          onClick={handleCreateEntry}
+          className={cn((isPreview || !session) && 'hidden')}
+        >
+          <PlusIcon size={14} className="mr-2" />
+          Add Entry
+        </Button>
+
         {session && (
           <>
             <Button
@@ -134,10 +162,26 @@ const TimelineTodo = () => {
         )}
       </div>
 
-      <ul className="my-4">
+      <ul className="my-4 mx-4">
         {sessionEntries.map((entry, index) => (
-          <li key={entry.id} className="my-2">
-            <div className="flex gap-2">
+          <li key={entry.id} className="my-2 relative">
+            {index < sessionEntries.length - 1 && (
+              <div
+                className="absolute top-7 -bottom-2 bg-slate-300 flex items-center justify-center"
+                style={{
+                  width: '1px',
+                  left: 7,
+                }}
+              >
+                <div className="px-0.5 bg-white rounded-lg border border-slate-300 text-xs text-gray-300">
+                  {dayjs
+                    .duration(entry.diff)
+                    .format(entry.diff > ms('1hour') ? 'H[h] m[m]' : 'm[m]')}
+                </div>
+              </div>
+            )}
+
+            <div className="flex gap-2 items-center">
               <div
                 className={cn(
                   'flex flex-col cursor-pointer',
@@ -168,51 +212,64 @@ const TimelineTodo = () => {
               <div className="font-bold text-purple-500 select-none">
                 {dayjs.duration(entry.time).format('HH:mm')}
               </div>
+              <PlusCircleIcon
+                size={13}
+                className={cn('cursor-pointer', isPreview && 'hidden')}
+                onClick={() => handleCreateTodo(entry.id)}
+              />
+              <XCircleIcon
+                className={cn('cursor-pointer', isPreview && 'hidden')}
+                onClick={() => deleteEntry(entry.id)}
+                size={13}
+              />
             </div>
-            <ul className="ml-6 my-2">
+            <ul className="ml-8 my-2">
               {todos
                 .filter((todo) => todo.entryId === entry.id)
                 .map((todo) => (
                   <li
                     key={todo.id}
-                    className={cn({
+                    className={cn('flex gap-2 items-center', {
                       'opacity-60': todo.completed,
                     })}
                   >
+                    <div
+                      className={cn(
+                        'flex flex-col cursor-pointer',
+                        isPreview && 'hidden'
+                      )}
+                    >
+                      <ChevronUpIcon
+                        size={15}
+                        onClick={() => reorderTodo(todo.id, 'up')}
+                        tabIndex={100 + index * 2}
+                      />
+                      <ChevronDownIcon
+                        size={15}
+                        tabIndex={101 + index * 2}
+                        onClick={() => reorderTodo(todo.id, 'down')}
+                      />
+                    </div>
                     <Checkbox
                       checked={todo.completed}
                       onCheckedChange={() => toggleTodo(todo.id)}
-                      className="mr-2"
                     />
-                    {todo.completed ? <s>{todo.text}</s> : todo.text}
+                    <span>
+                      {todo.completed ? <s>{todo.text}</s> : todo.text}
+                    </span>
+                    <XCircleIcon
+                      className={cn('cursor-pointer', isPreview && 'hidden')}
+                      onClick={() => deleteTodo(todo.id)}
+                      size={13}
+                    />
                   </li>
                 ))}
-
-              <li className={cn(isPreview && 'hidden')}>
-                <Button
-                  variant="secondary"
-                  size="sm"
-                  onClick={() => handleCreateTodo(entry.id)}
-                >
-                  <PlusIcon size={14} className="mr-2" />
-                  Add Todo
-                </Button>
-              </li>
             </ul>
           </li>
         ))}
       </ul>
-
-      {session && (
-        <div className={cn(isPreview && 'hidden')}>
-          <Button variant="secondary" size="sm" onClick={handleCreateEntry}>
-            <PlusIcon size={14} className="mr-2" />
-            Add Entry
-          </Button>
-        </div>
-      )}
     </div>
   );
 };
 
-export default withStoreLoading(TimelineTodo, useStore, 'isLoading');
+export default TimelineTodo;
--- src/apps/TimelineTodo/store.ts
+++ src/apps/TimelineTodo/store.ts
@@ -4,6 +4,7 @@ import {
 } from '@/utils/zustand/gsheet-storage';
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
+import { immer } from 'zustand/middleware/immer';
 
 type StoreType = {
   isLoading: boolean;
@@ -16,6 +17,7 @@ type StoreType = {
     id: string;
     sessionId: string;
     time: number;
+    tags: string[];
   }>;
 
   todos: Array<{
@@ -34,98 +36,130 @@ type StoreType = {
   deleteSession: (id: string) => void;
   deleteEntry: (id: string) => void;
   deleteTodo: (id: string) => void;
+  reorderTodo: (id: string, direction: 'up' | 'down') => void;
 };
 
 const uid = () => Math.random().toString(36).substring(2, 9);
 
 export const useStore = create<StoreType>()(
-  persist(
-    (set) =>
-      ({
-        isLoading: true,
-        entries: [],
-        todos: [],
-        sessions: [],
-        createSession: (name) =>
-          set((prev) => ({
-            sessions: [
-              ...prev.sessions,
-              {
-                id: uid(),
-                name,
-              },
-            ],
-          })),
+  immer(
+    persist(
+      (set) =>
+        ({
+          isLoading: true,
+          entries: [],
+          todos: [],
+          sessions: [],
+          createSession: (name) =>
+            set((prev) => ({
+              sessions: [
+                ...prev.sessions,
+                {
+                  id: uid(),
+                  name,
+                },
+              ],
+            })),
 
-        createEntry: (sessionId) =>
-          set((prev) => ({
-            entries: [
-              ...prev.entries,
-              {
-                id: uid(),
-                sessionId,
-                time:
-                  (prev.entries[prev.entries.length - 1]?.time || 0) +
-                  1000 * 60 * 5,
-              },
-            ],
-          })),
+          createEntry: (sessionId) =>
+            set((prev) => ({
+              entries: [
+                ...prev.entries,
+                {
+                  id: uid(),
+                  sessionId,
+                  time:
+                    (prev.entries[prev.entries.length - 1]?.time || 0) +
+                    1000 * 60 * 5,
+                  tags: [],
+                },
+              ],
+            })),
 
-        updateEntryTime: (id, time) =>
-          set((prev) => ({
-            entries: prev.entries.map((entry) =>
-              entry.id === id
-                ? {
-                    ...entry,
-                    time,
-                  }
-                : entry
-            ),
-          })),
+          updateEntryTime: (id, time) =>
+            set((prev) => ({
+              entries: prev.entries.map((entry) =>
+                entry.id === id
+                  ? {
+                      ...entry,
+                      time,
+                    }
+                  : entry
+              ),
+            })),
 
-        createTodo: (entryId, text) =>
-          set((prev) => ({
-            todos: [
-              ...prev.todos,
-              {
-                id: uid(),
-                entryId,
-                text,
-                completed: false,
-              },
-            ],
-          })),
+          createTodo: (entryId, text) =>
+            set((prev) => ({
+              todos: [
+                ...prev.todos,
+                {
+                  id: uid(),
+                  entryId,
+                  text,
+                  completed: false,
+                },
+              ],
+            })),
 
-        toggleTodo: (id) =>
-          set((prev) => ({
-            todos: prev.todos.map((todo) =>
-              todo.id === id
-                ? {
-                    ...todo,
-                    completed: !todo.completed,
-                  }
-                : todo
-            ),
-          })),
-        deleteSession: (id) =>
-          set((prev) => ({
-            sessions: prev.sessions.filter((session) => session.id !== id),
-          })),
-        deleteEntry: (id) =>
-          set((prev) => ({
-            entries: prev.entries.filter((session) => session.id !== id),
-          })),
-        deleteTodo: (id) =>
-          set((prev) => ({
-            todos: prev.todos.filter((session) => session.id !== id),
-          })),
-      } as StoreType),
-    {
-      name: 'timeline-todo',
-      storage: createJSONStorage(() =>
-        gSheetStorage('1gI4tbIt1ETMm6aPXNdk5ycHt8tHlJr-TxkbKuAKqBKc')
-      ),
-    }
+          toggleTodo: (id) =>
+            set((prev) => ({
+              todos: prev.todos.map((todo) =>
+                todo.id === id
+                  ? {
+                      ...todo,
+                      completed: !todo.completed,
+                    }
+                  : todo
+              ),
+            })),
+          deleteSession: (id) =>
+            set((prev) => ({
+              sessions: prev.sessions.filter((session) => session.id !== id),
+            })),
+          deleteEntry: (id) =>
+            set((prev) => ({
+              entries: prev.entries.filter((session) => session.id !== id),
+            })),
+          deleteTodo: (id) =>
+            set((prev) => ({
+              todos: prev.todos.filter((session) => session.id !== id),
+            })),
+          reorderTodo: (id, direction: 'up' | 'down') => {
+            set((prev) => {
+              const todo = prev.todos.find((todo) => todo.id === id);
+              const entry = prev.entries.find(
+                (entry) => entry.id === todo?.entryId
+              );
+              const entryTodos = prev.todos.filter(
+                (todo) => todo.entryId === entry?.id
+              );
+
+              if (!entry || !todo) {
+                return prev;
+              }
+
+              const todoIndex = entryTodos.indexOf(todo);
+              prev.todos.splice(todoIndex, 1);
+
+              if (direction === 'up') {
+                const prevItem = entryTodos[todoIndex - 1];
+                const prevItemIndex = prev.todos.indexOf(prevItem);
+                prev.todos.splice(prevItemIndex, 0, todo);
+              } else {
+                const nextItem = entryTodos[todoIndex + 1];
+                const nextItemIndex = prev.todos.indexOf(nextItem);
+                prev.todos.splice(nextItemIndex + 1, 0, todo);
+              }
+            });
+          },
+        } as StoreType),
+      {
+        name: 'timeline-todo',
+        storage: createJSONStorage(() =>
+          gSheetStorage('1gI4tbIt1ETMm6aPXNdk5ycHt8tHlJr-TxkbKuAKqBKc')
+        ),
+      }
+    )
   )
 );
 
