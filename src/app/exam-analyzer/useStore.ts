d41c04e71091906ec0a799614fcc6be73cccc512
Yasin <yasin.tazeoglu@osapiens.com>
2025-07-06 20:25:20 +0300

    v1

--- src/app/exam-analyzer/useStore.ts
+++ src/app/exam-analyzer/useStore.ts
@@ -8,6 +8,8 @@ import { gSheetStorageDeprecated } from '@/utils/zustand/gsheet-storage';
 type StoreType = {
   exams: ReturnType<typeof parseExam>[];
   addExam: (exam: ReturnType<typeof parseExam>) => void;
+  updateExam: (id: string, exam: ReturnType<typeof parseExam>) => void;
+  deleteExam: (id: string) => void;
 };
 
 export const useStore = create(
@@ -15,6 +17,16 @@ export const useStore = create(
     (set) => ({
       exams: [],
       addExam: (exam) => set((prev) => ({ exams: prev.exams.concat(exam) })),
+      updateExam: (id, updatedExam) => 
+        set((prev) => ({
+          exams: prev.exams.map((exam) => 
+            exam.id === id ? updatedExam : exam
+          )
+        })),
+      deleteExam: (id) => 
+        set((prev) => ({
+          exams: prev.exams.filter((exam) => exam.id !== id)
+        })),
     }),
     {
       name: 'smy_exams',
